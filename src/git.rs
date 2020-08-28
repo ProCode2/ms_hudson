54d42b3e6fcbe9b4fabd8ae6d9e0b9f9aa10e0bf
ProCode2 <bpro249@gmail.com>
2020-08-28 16:51:49 +0000

    fixed git.rs

--- src/git.rs
+++ src/git.rs
@@ -1,22 +1,30 @@
-use reqwest::header::{ ACCEPT, AUTHORIZATION, CONTENT_TYPE};
+use reqwest::header::{ USER_AGENT };
 use dotenv::dotenv;
-use reqwest::Error;
 use reqwest::Client;
+use reqwest::Error;
 use std::collections::HashMap;
 use std::env;
 
 pub async fn make_github_repo(filename: &str) -> Result<(), Error> {
+    //setting dotenv variables to env variables
     dotenv().expect("Failed to set env");
     let token = env::var("GITHUB_ACCESS_TOKEN").unwrap();
-    let mut map = HashMap::new();
-    map.insert("name", filename);
+    let username = env::var("GITHUB_USERNAME").unwrap();
+    println!("{}:{}", username, token);
+
     let client = Client::new();
-    let body = client.post("https://api.github.com/users/repos/")
-        .header(ACCEPT, "application/vnd.github.v3+json")
-        .header(AUTHORIZATION, format!("token {}", token))
-        .json(&map)
+
+    let mut json_map = HashMap::new();
+    json_map.insert("name", filename);
+
+    let res = client.post("https://api.github.com/user/repos")
+        .basic_auth(&username, Some(token))
+        .header(USER_AGENT, &username)
+        .json(&json_map)
         .send()
+        .await?
+        .text()
         .await?;
-    println!("{:?}", body);
+    println!("{:?}", res);
     Ok(())
 }
