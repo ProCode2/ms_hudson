00b8b97ef7f20031f59981dbd00f657ca9264a22
ProCode2 <bpro249@gmail.com>
2020-09-04 11:11:03 +0000

    initialise gjt repo

--- src/git.rs
+++ src/git.rs
@@ -4,10 +4,11 @@ use reqwest::Client;
 use reqwest::Error;
 use std::collections::HashMap;
 use std::env;
+use std::process::Command;
 
 pub async fn make_github_repo(filename: &str) -> Result<(), Error> {
     //setting dotenv variables to env variables
-    dotenv().expect("Failed to set env");
+    /*dotenv().expect("Failed to set env");
     let token = env::var("GITHUB_ACCESS_TOKEN").unwrap();
     let username = env::var("GITHUB_USERNAME").unwrap();
     println!("{}:{}", username, token);
@@ -26,5 +27,17 @@ pub async fn make_github_repo(filename: &str) -> Result<(), Error> {
         .text()
         .await?;
     println!("{:?}", res);
+    */
+    let cd_output = Command::new("git")
+        .arg("init")
+        .arg("newproject")
+        .output()
+        .expect("Could not process cd command");
+
+    let output = match String::from_utf8(cd_output.stdout) {
+        Ok(y) => y,
+        Err(e) => e.to_string()
+    };
+    println!("{}", output);
     Ok(())
 }
